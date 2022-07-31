9874f3452b0eafaa288ab9a97c581d1e89fa3a65
unknown <buragohain4@gmail.com>
2022-07-31 19:05:00 +0530

    Generate CouponNo

---
+++ CouponNo/CouponNo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace CouponNo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+            var stringChars = new char[15];
+
+            for (int i = 0; i < stringChars.Length; i++)
+            {
+                stringChars[i] = chars[random.Next(chars.Length)];
+                Console.WriteLine(" Your Coupon Number is: " + stringChars[i]);
+
+            }
+        }
+    }
+}
