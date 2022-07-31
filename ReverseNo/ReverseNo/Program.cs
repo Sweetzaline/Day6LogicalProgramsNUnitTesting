c3d4749e62acff6c438477cee3388d17528d202e
unknown <buragohain4@gmail.com>
2022-07-31 19:00:17 +0530

    Find Reverse of a number

---
+++ ReverseNo/ReverseNo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace ReverseNo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, temp, remainder, reverse = 0;
+
+            Console.WriteLine("Enter an integer \n");
+
+            num = int.Parse(Console.ReadLine());
+
+            temp = num;
+            while (num > 0)
+            {
+                remainder = num % 10;
+                reverse = reverse * 10 + remainder;
+                num /= 10;
+            }
+           
+            Console.WriteLine("The reverse of the number is = {0}", reverse);
+            Console.Read();
+        }
+    }
+}
