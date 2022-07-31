75c12ff53c0b80faa8a27a44b000d499132c6501
unknown <buragohain4@gmail.com>
2022-07-31 20:16:17 +0530

    Check monthly Pay

---
+++ monthlypay/monthlypay/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace monthlypay
+{
+    class monthlyPayment
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Principal Loan amount: ");
+            double p = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Years: ");
+            double y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the percent interest compounded: ");
+            double R = Convert.ToInt32(Console.ReadLine());
+
+            double n = 12 * y;
+            double r = R / (12 * 100);
+            double payment = p * r / (1 - Math.Pow((1 + r), -n));
+            Console.WriteLine(payment + "is monthly payment");
+        }
+       
+
+    }
+}
