3d810a2390403493ac3c340b6cffdcc792cc3be2
unknown <buragohain4@gmail.com>
2022-07-31 18:51:31 +0530

    Check Prime No.

---
+++ PrimeNo/PrimeNo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace PrimeNo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+            Program.prime(y);
+        }
+        public static void prime(int num)
+        {
+            for (int i = 2; i <= (num/2); i++)
+            {
+                if (num % i == 0)
+                {
+                    Console.WriteLine($"{i} is not a prime no {num}");
+                }
+                else
+                {
+                    Console.WriteLine($"{num} is a prime no ");
+                }
+            }
+
+        }
+    }
+}
