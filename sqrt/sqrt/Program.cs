bafbf3a648ff664aa622716683cef78886d7bfda
unknown <buragohain4@gmail.com>
2022-07-31 20:22:19 +0530

    Find Square Root of a number

---
+++ sqrt/sqrt/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace sqrt
+{
+    class Sqrt
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            int c = Convert.ToInt32(Console.ReadLine());
+            double t = c;
+            double epsilon = 1e-15;
+            //check for negative number
+
+
+            if (t > 0)
+            {
+                //check condition till (t - c/t) becomes less then (epsilon * t)
+                while (Math.Abs(t - c / t) > epsilon * t)
+                {
+                    t = (t + c / t) / 2.0;  //formula for square root.
+                }
+                Console.WriteLine("Absoulte square root is: " + t);
+            }
+            else
+                Console.WriteLine("Enter positive number");
+        }
+    }
+}
