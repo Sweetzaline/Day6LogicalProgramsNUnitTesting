09a4253078743e884dd230be0d7a2a251149ee5b
unknown <buragohain4@gmail.com>
2022-07-31 18:34:32 +0530

    Check for Perfect No

---
+++ PerfectNo/PerfectNo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+
+namespace UC2_PerfectNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, i, sum;
+
+            Console.Write("\n\n");
+            Console.Write("Check whether a number is perfect number:\n");
+            Console.Write("\n\n");
+
+            Console.Write("Enter the  number : ");
+            n = Convert.ToInt32(Console.ReadLine());
+            sum = 0;
+            {
+                Console.Write("The positive divisor  : ");
+            }
+
+            for (i = 1; i < n; i++)
+            {
+                if (n % i == 0)
+                {
+                    sum = sum + i;
+                    Console.Write("{0}  ", i);
+                }
+            }
+            Console.Write("\nThe sum of the divisor is : {0}", sum);
+            if (sum == n)
+                Console.Write("\n, the number is perfect.");
+            else
+                Console.Write("\n, the number is not perfect.");
+            Console.Write("\n");
+        }
+    }
+}
