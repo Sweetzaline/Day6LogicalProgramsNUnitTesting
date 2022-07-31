44f0365407fa23cf106c60afd6e8ff367d7510dd
unknown <buragohain4@gmail.com>
2022-07-31 19:12:35 +0530

    StopWatch Simulator

---
+++ StopWatch/StopWatch/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+namespace StopWatch
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            // ... This takes 10 seconds to finish.
+            for (int i = 0; i < 1000; i++)
+            {
+                System.Threading.Thread.Sleep(10);
+            }
+
+            // Stop.
+            stopwatch.Stop();
+
+            // Write hours, minutes and seconds.
+            Console.WriteLine("Time passed is of: {0:hh\\:mm\\:ss}", stopwatch.Elapsed);
+        }
+    }
+}
