bc0f287cfbfacb2178a198c04769bfb139b54eee
unknown <buragohain4@gmail.com>
2022-07-31 19:39:44 +0530

    Show Day of Week

---
+++ DayofWeek/DayofWeek/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace DayofWeek
+{
+    class Sample
+    {
+     public static void Main()
+     {
+        
+        DateTime dt = new DateTime(2022, 7, 31);
+        Console.WriteLine("Is Thursday the day of the week for {0:d}?: {1}",
+                           dt, dt.DayOfWeek == DayOfWeek.Thursday);
+        Console.WriteLine("The day of the week for {0:d} is {1}.", dt, dt.DayOfWeek);
+     }
+    }
+}
